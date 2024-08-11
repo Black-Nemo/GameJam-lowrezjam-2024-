58c8762e6243c9b9024c90ed6f28f7c6a98c2469
Numan Özdemir <nmnzdmr35@gmail.com>
2024-08-12 00:48:34 +0300

    Vırus IntoTheHuman and OutTheHuman

---
+++ Assets/_Workspaces/Numan/Scripts/Character/CharacterInfectedSystem.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterInfectedSystem : MonoBehaviour
+{
+    public float infectionTime;
+
+    public bool isIntoTheHuman;
+    public SpriteRenderer spriteRenderer;
+    public Collider2D myCollider2D;
+
+    public GameObject IntoTheHumanEnterCanvas;
+
+    private GameObject _human;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isIntoTheHuman) { return; }
+        if (other.tag == "Enemy")
+        {
+            if (other.TryGetComponent(out Enemy enemy) && !enemy.isInfected)
+            {
+                IntoTheHumanEnterCanvas.SetActive(true);
+                _human = other.gameObject;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (isIntoTheHuman) { return; }
+        if (other.tag == "Enemy")
+        {
+            if (_human == other.gameObject && !isIntoTheHuman)
+            {
+                IntoTheHumanEnterCanvas.SetActive(false);
+                _human = null;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (_human != null && Input.GetKeyDown(KeyCode.E) && !_human.GetComponent<Enemy>().isInfected)
+        {
+            if (!isIntoTheHuman)
+            {
+                IntoTheHuman();
+            }
+        }
+
+        if (isIntoTheHuman)
+        {
+            transform.position = _human.transform.position;
+        }
+    }
+    private void IntoTheHuman()
+    {
+        IntoTheHumanEnterCanvas.SetActive(false);
+        isIntoTheHuman = true;
+        spriteRenderer.enabled = false;
+        myCollider2D.enabled = false;
+        _human.GetComponent<Enemy>().slider.gameObject.SetActive(true);
+        _human.GetComponent<Enemy>().slider.value = 0;
+        _human.GetComponent<Enemy>().sliderImage.color = Color.green;
+        _human.GetComponent<Enemy>().slider.maxValue = infectionTime;
+        StartCoroutine(infectionEnumator());
+    }
+    private void OutTheHuman()
+    {
+        isIntoTheHuman = false;
+        spriteRenderer.enabled = true;
+        myCollider2D.enabled = true;
+        _human.GetComponent<Enemy>().slider.gameObject.SetActive(false);
+    }
+
+    float _waitTimer = 0;
+    IEnumerator infectionEnumator()
+    {
+        while (true)
+        {
+            if (_waitTimer >= infectionTime)
+            {
+                _human.GetComponent<Enemy>().isInfected = true;
+                _waitTimer = 0;
+                OutTheHuman();
+                break;
+            }
+            _human.GetComponent<Enemy>().slider.value = _waitTimer;
+            _waitTimer += 0.1f;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
--- Assets/_Workspaces/Numan/Scripts/Enemy/Enemy.cs
+++ Assets/_Workspaces/Numan/Scripts/Enemy/Enemy.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
@@ -10,8 +10,9 @@ public class Enemy : MonoBehaviour
 
     public GameObject infectedSpriteObject;
     public GameObject notInfectedSpriteObject;
+    public Slider slider;
+    public Image sliderImage;
 
-    public bool RandomMove;
     public string tagName;
 
 
@@ -24,7 +25,6 @@ public class Enemy : MonoBehaviour
     //private GameObject player;
     NavMeshAgent agent;
 
-    public List<MoveNode> moveNodes;
 
     public List<MoveNode> moveRandomNodes;
 
@@ -38,73 +38,28 @@ public class Enemy : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
         //player = GameObject.FindWithTag("Player");
 
-        _moveNode = moveNodes[0];
-        if (RandomMove)
-        {
-            GameObject[] moveNodesObject = GameObject.FindGameObjectsWithTag(tagName);
-            foreach (var item in moveNodesObject)
-            {
-                moveRandomNodes.Add(new MoveNode() { waitTime = 1, transform = item.transform });
-            }
-            StartCoroutine(enumeratorMoveRandom());
-        }
-        else
-        {
-            StartCoroutine(enumeratorMove());
-        }
 
+        GameObject[] moveNodesObject = GameObject.FindGameObjectsWithTag(tagName);
+        foreach (var item in moveNodesObject)
+        {
+            moveRandomNodes.Add(new MoveNode() { waitTime = 1, transform = item.transform });
+        }
+        StartCoroutine(enumeratorMoveRandom());
     }
 
     private MoveNode _moveNode;
     int _counter = 0;
     float _waitTimer = 0;
-    IEnumerator enumeratorMove()
-    {
-        Rotate(_moveNode.transform.position);
-        while (true)
-        {
-            //Vector2 direction = (fov.player.position - transform.position).normalized;
-            //rb2d.velocity = direction * moveSpeed;
-            agent.SetDestination(_moveNode.transform.position);
-            if (fov.player != null) { _moveNode = null; break; }
-            if (Vector2.Distance(transform.position, _moveNode.transform.position) < 0.25f)
-            {
-                //agent.isStopped = true;
-                if (_waitTimer >= _moveNode.waitTime)
-                {
-                    _waitTimer = 0;
-
-                    if (moveNodes[moveNodes.Count - 1].transform == _moveNode.transform)
-                    {
-                        _moveNode = moveNodes[0];
-                        _counter = 0;
-                    }
-                    else
-                    {
-                        _counter++;
-                        _moveNode = moveNodes[_counter];
-                    }
-                    Rotate(_moveNode.transform.position);
-                }
-                else
-                {
-                    _waitTimer += 0.1f;
-                }
-
-            }
-            else
-            {
-                //agent.isStopped = false;
-            }
-            yield return new WaitForSeconds(0.1f);
-        }
-
-    }
     IEnumerator enumeratorMoveRandom()
     {
-        Rotate(_moveNode.transform.position);
         while (true)
         {
+            if (_moveNode == null)
+            {
+                int rnd = Random.Range(0, moveRandomNodes.Count);
+                _moveNode = moveRandomNodes[rnd];
+                Rotate(_moveNode.transform.position);
+            }
             //Vector2 direction = (fov.player.position - transform.position).normalized;
             //rb2d.velocity = direction * moveSpeed;
             agent.SetDestination(_moveNode.transform.position);
@@ -175,7 +130,7 @@ public class Enemy : MonoBehaviour
         }
         else if (_moveNode == null)
         {
-            _moveNode = null;
+            StartCoroutine(enumeratorMoveRandom());
         }
         else
         {
@@ -186,18 +141,52 @@ public class Enemy : MonoBehaviour
     {
         if (fov.player != null && fov.player.gameObject == other.gameObject)
         {
-            other.gameObject.GetComponent<Enemy>().isInfected = false;
-            fov.player = null;
-            StartCoroutine(enumeratorMoveRandom());
+            other.gameObject.GetComponent<Enemy>().slider.gameObject.SetActive(true);
+            other.gameObject.GetComponent<Enemy>().slider.value = 0;
+            other.gameObject.GetComponent<Enemy>().slider.maxValue = infectionReverseTime;
+            other.gameObject.GetComponent<Enemy>().sliderImage.color = Color.red;
+            StartCoroutine(infectionReverseEnumator(other.gameObject));
         }
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (fov.player != null && fov.player.gameObject == other.gameObject)
         {
-            other.gameObject.GetComponent<Enemy>().isInfected = false;
-            fov.player = null;
-            StartCoroutine(enumeratorMoveRandom());
+            other.gameObject.GetComponent<Enemy>().slider.gameObject.SetActive(true);
+            other.gameObject.GetComponent<Enemy>().slider.value = 0;
+            other.gameObject.GetComponent<Enemy>().slider.maxValue = infectionReverseTime;
+            other.gameObject.GetComponent<Enemy>().sliderImage.color = Color.red;
+            StartCoroutine(infectionReverseEnumator(other.gameObject));
+        }
+    }
+
+    public float infectionReverseTime;
+    float _waitTimerInfectionReverse = 0;
+    IEnumerator infectionReverseEnumator(GameObject other)
+    {
+        while (true)
+        {
+            if (_waitTimerInfectionReverse >= infectionReverseTime)
+            {
+                other.gameObject.GetComponent<Enemy>().slider.gameObject.SetActive(false);
+                other.gameObject.GetComponent<Enemy>().isInfected = false;
+                fov.player = null;
+                _moveNode = null;
+                _waitTimerInfectionReverse = 0;
+                break;
+            }
+            else if (isInfected)
+            {
+                other.gameObject.GetComponent<Enemy>().slider.gameObject.SetActive(false);
+                other.gameObject.GetComponent<Enemy>().isInfected = true;
+                fov.player = null;
+                _moveNode = null;
+                _waitTimerInfectionReverse = 0;
+                break;
+            }
+            other.gameObject.GetComponent<Enemy>().slider.value = _waitTimerInfectionReverse;
+            _waitTimerInfectionReverse += 0.1f;
+            yield return new WaitForSeconds(0.1f);
         }
     }
 }
